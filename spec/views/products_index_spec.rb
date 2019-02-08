b96335131651c347e820251ee26911aebe91eef2
Rudy Ravelin <rudy@noukod.com>
2019-02-08 15:34:24 +0000

    Automatically backed up by Learn

--- spec/views/products_index_spec.rb
+++ spec/views/products_index_spec.rb
@@ -1,5 +1,5 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe "products/index", :type => :view do
 #   before do
 #     controller.singleton_class.class_eval do
@@ -10,7 +10,7 @@
 #         helper_method :cart
 #     end
 #   end
-# 
+#
 #   it "shows everything in the cart" do
 #     session[:cart] = ['apples', 'bananas', 'pears']
 #     render
